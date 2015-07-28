171126971b11761b0fbf5082eca6f3b807f18935
scorpianz1525 <jacquesrascagneres@gmail.com>
2015-07-28 13:25:12 +0100

    map start (just so i can sync)

--- digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment3.java
+++ digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment3.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * Created by Isaac on 28/07/2015.
  */
 public class
-        Fragment3 extends NavigationControl.PlaceholderFragment{
+        Fragment3 extends NavigationControl.PlaceholderFragment implements OnMapReadyCallback{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
