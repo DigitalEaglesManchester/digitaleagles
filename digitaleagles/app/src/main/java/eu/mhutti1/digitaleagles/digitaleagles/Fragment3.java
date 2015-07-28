1e0d2468df067838b5f23bea138204f012ad06e5
scorpianz1525 <jacquesrascagneres@gmail.com>
2015-07-28 14:03:55 +0100

    im so bad at coding :(

--- digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment3.java
+++ digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment3.java
@@ -4,16 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.*;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Isaac on 28/07/2015.
  */
-public class
-        Fragment3 extends NavigationControl.PlaceholderFragment implements OnMapReadyCallback{
+
+public class Fragment3 extends NavigationControl.PlaceholderFragment implements OnMapReadyCallback {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView;
         rootView = inflater.inflate(R.layout.screen3, container, false);
         return rootView;
@@ -21,6 +35,7 @@ public class
     @Override
     public void onResume() {
         super.onResume();
-
+        GoogleMap  mapFragment = (GoogleMap) thisActivity.findViewById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 }
