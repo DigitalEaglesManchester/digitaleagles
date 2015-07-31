00db291d540c1e38aa7442ca8d40282f9d52ddbb
scorpianz1525 <jacquesrascagneres@gmail.com>
2015-07-31 17:27:17 +0100

    Screen 4 (Search screen) Setup

--- digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/NavigationControl.java
+++ digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/NavigationControl.java
@@ -74,17 +74,15 @@ public class NavigationControl extends ActionBarActivity
         switch (number) {
             case 1:
                 mTitle = getString(R.string.title_section1);
-
-
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
-
-
                 break;
             case 3:
                 mTitle = getString(R.string.title_section3);
-
+                break;
+            case 4:
+                mTitle = "Search";
                 break;
         }
     }
@@ -138,6 +136,9 @@ public class NavigationControl extends ActionBarActivity
                     break;
                 case 3:
                     fragment = new Fragment3();
+                    break;
+                case 4:
+                    fragment = new Fragment4();
                     break;
                 default:
                     fragment = new Fragment1();
