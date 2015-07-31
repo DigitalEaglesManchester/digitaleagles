b5262fbd937346867d1e250e1aeba6f60803f1be
scorpianz1525 <jacquesrascagneres@gmail.com>
2015-07-31 17:52:01 +0100

    Soo.. How to add search

--- digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment4.java
+++ digitaleagles/app/src/main/java/eu/mhutti1/digitaleagles/digitaleagles/Fragment4.java
@@ -2,7 +2,9 @@ package eu.mhutti1.digitaleagles.digitaleagles;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -15,22 +17,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Created by Isaac on 28/07/2015.
+ * Created by Jacques on 31/07/2015.
  */
 public class Fragment4 extends NavigationControl.PlaceholderFragment {
-    ListView list;
-    ArrayList<String> textList;
-    ArrayAdapter<String> listAdapter;
-    int[] textIds;
-    String[] datetime;
-    ArrayList<DBResponseBean> beans;
-    public DatabaseHandler dataService;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView;
         rootView = inflater.inflate(R.layout.screen4, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
     }
 
 
